16737694866a649b6b4de84758455a9b50b9daf5
Donghoon Shin <dkxmffktm@gmail.com>
2015-07-19 18:00:11 +0900

    2주 Chap.8-4

--- DH Shin/2/Chap.8-4/Chap.8-4/Source.cpp
+++ DH Shin/2/Chap.8-4/Chap.8-4/Source.cpp
@@ -1,6 +1,54 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#define MAX_NUM 3
+
+bool Check();
 
 int main()
 {
+	srand(time(NULL));
+
+	int input = 1;
+	while (input != 0)
+	{
+		std::cin >> input;
+		if (Check())
+		{
+			std::cout << "You win." << std::endl;
+		}
+		else
+		{
+			std::cout << "You lose." << std::endl;
+		}
+	}
+
 	return 0;
+}
+
+bool Check()
+{
+	bool result = true;
+	int randNum[MAX_NUM] = { 0 };
+
+	for (int i = 0; i < MAX_NUM; ++i)
+	{
+		randNum[i] = rand() % 10;
+		std::cout << randNum[i];
+	}
+	std::cout << std::endl;
+
+	int num = randNum[0];
+
+	for (int i = 1; i < MAX_NUM; ++i)
+	{
+		if (num != randNum[i])
+		{
+			result = false;
+			break;
+		}
+	}
+
+	return result;
 }
