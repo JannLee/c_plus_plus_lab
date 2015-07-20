e8ee4dc261b2743c15460933879590fe48bb2112
백병민 <qkfl4@naver.com>
2015-07-20 13:12:43 +0900

    백병민 : 파일 잘못 커밋됨

--- CppStudyForBBM/chapter5/main.cpp
+++ CppStudyForBBM/chapter5/main.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <ctime>
 
 using namespace std;
 
 int main() {
-	srand(time(NULL));
-	int a = rand() % 10;
-	int b = rand() % 10;
-	int c = rand() % 10;
-	cout << a << endl;
-	cout << b << endl;
-	cout << c << endl;
+	string q = "";
+	string one = "";
+	string two = "";
+	string three = "";
 
-	if (a == b && b == c) {
-		cout << "WIN!!!" << endl;
+	int ione = 0;
+	int itwo = 0;
+	int ithree = 0;
+	int in = -1;
+
+	cout << "설문을 입력해 주세요." << endl;
+	cin >> q;
+	cout << "첫 번째, 문항을 입력해 주세요. : ";
+	cin >> one;
+	cout << "두 번째, 문항을 입력해 주세요. : ";
+	cin >> two;
+	cout << "세 번째, 문항을 입력해 주세요. : ";
+	cin >> three;
+	while (in != 0) {
+		cout << "투표해주세요." << endl;
+		cin >> in;
+		if (in == 1) {
+			++ione;
+		}
+		else if (in == 2) {
+			++itwo;
+		}
+		else if (in == 3) {
+			++ithree;
+		}
 	}
-	else {
-		cout << "LOSE!!" << endl;
+	cout << one << "\t: ";
+	for (int i = 0; i < ione; i++) {
+		cout << "□";
 	}
+	cout << endl;
+	cout << two << "\t: ";
+	for (int i = 0; i < itwo; i++) {
+		cout << "□";
+	}
+	cout << endl;
+	cout << three << "\t: ";
+	for (int i = 0; i < ithree; i++) {
+		cout << "□";
+	}
+	cout << endl;
+	cin.get();
 	return 0;
 }
