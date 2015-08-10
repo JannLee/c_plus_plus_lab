85448c9709325a5bd948d2d1203658b3b671951e
원권 최 <slayas@naver.com>
2015-08-10 21:45:28 +0900

    9:45

--- wk/chap_18_01/wk.cpp
+++ wk/chap_18_01/wk.cpp
@@ -4,10 +4,11 @@
 
 using namespace std;
 
-void add(map<string, string>& AddressBook);
-void del(map<string, string>& AddressBook);
+void insert(map<string, string>& AddressBook);
+void erase(map<string, string>& AddressBook);
 void update(map<string, string>& AddressBook);
 void search(map<string, string>& AddressBook);
+
 
 int main()
 {
@@ -17,20 +18,21 @@ int main()
 	while (input < 5)
 	{
 		cout << "Select command" << endl;
-		cout << "1 : add" << endl;
-		cout << "2 : del" << endl;
-		cout << "3 : update" << endl;
-		cout << "4 : search" << endl;
-		cout << "5 : exit" << endl;
+		cout << "1 : Insert" << endl;
+		cout << "2 : Erase" << endl;
+		cout << "3 : Update" << endl;
+		cout << "4 : Search" << endl;
+		cout << "5 : Exit" << endl;
 		cin >> input;
 
 		switch (input)
 		{
+			system("cls");
 			case 1:
-				add(AddressBook);
+				insert(AddressBook);
 				break;
 			case 2:
-				del(AddressBook);
+				erase(AddressBook);
 				break;
 			case 3:
 				update(AddressBook);
@@ -45,22 +47,70 @@ int main()
 	
 }
 
-void add(map<string, string>& AddressBook)
+void insert(map<string, string>& AddressBook)
 {
-	cout << "11" <<endl;
+	string name;
+	string email;
+
+	cout << "Name ? : ";
+	cin >> name;
+	cout << "Email ? : ";
+	cin >> email;
+
+	AddressBook.insert(make_pair(name, email));
+
+	system("cls");
 }
 
-void del(map<string, string>& AddressBook)
+void erase(map<string, string>& AddressBook)
 {
-	cout << "11" << endl;
+	string name;
+	cout << "Name ? : ";
+	cin >> name;
+
+	AddressBook.erase(name);
+
+	system("cls");
 }
 
 void update(map<string, string>& AddressBook)
 {
-	cout << "11" << endl;
+	string name;
+	string email;
+
+	cout << "Name ? : ";
+	cin >> name;
+	cout << "Email ? : ";
+	cin >> email;
+
+	system("cls");
+
+	map<string, string>::iterator itr = AddressBook.find(name);
+	if (itr != AddressBook.end())
+	{
+		itr->second = email;	 		
+	}
+	else
+	{
+		cout << "can't find name.." << endl;
+	}
 }
 
 void search(map<string, string>& AddressBook)
 {
-	cout << "11" << endl;
+	string name;
+	cout << "Name ? : ";
+	cin >> name;
+
+	map<string, string>::const_iterator itr = AddressBook.find(name);
+
+	if (itr != AddressBook.end())
+	{
+		cout << "Name : " << itr->first << endl;
+		cout << "Email : " << itr->second << endl;
+	}
+	else
+	{
+		cout << "cnat find name.." << endl;
+	}
 }
