bbc34f807f6b6a2248c62753bf84270fe238376b
원권 최 <slayas@naver.com>
2015-08-24 19:56:39 +0900

    hehehehe

--- wk/chap_25_01/vectorOfInt.h
+++ wk/chap_25_01/vectorOfInt.h
@@ -4,8 +4,8 @@ public:
 	vectorOfInt(); // 32개의 요소 벡터를 할당하는 무인수 생성자
 	vectorOfInt(int size); // 초기 크기를 인수로 받는 생성자
 	virtual ~vectorOfInt(); // 소멸자
-	vectorOfInt& operator=(const vectorOfInt& other); // 대입 연산자
-	vectorOfInt(const vectorOfInt& other); // 복사 생성자
+	vectorOfInt &operator=(const vectorOfInt &other); // 대입 연산자
+	vectorOfInt(const vectorOfInt &other); // 복사 생성자
 				
 	int get(int index);
 	void set(int index, int value);
@@ -14,6 +14,6 @@ public:
 	void print_array();
 
 private:
-	int* data;
+	int *data;
 	int size;
 };
